b8a7b2a68732cc63ca8c2004b003bbb5c8f54eb0
Nguyen Cuong <ntcuong777@gmail.com>
2025-07-22 01:01:24 +0700

    add new submodule

---
+++ and-listp-dotemacs/.config/emacs/disabled.el
@@ -0,0 +1,7 @@
+;; -*- lexical-binding: t -*-
+
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+(put 'narrow-to-page 'disabled nil)
+(put 'dired-find-alternate-file 'disabled nil)
